6913c8cdf1f4ee0dd38156e43191b73d074c2304
Andrei Pliukhaev <pluhmylkin@me.com>
2018-10-25 23:38:14 +0700

    Initial  app

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Section from "./components/Section";
+
+class App extends Component {
+  render() {
+    return <Section />;
+  }
+}
+
+export default App;
