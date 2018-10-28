70083d3ee8938f30dadac511ea781bbdfdfbed55
Andrei Pliukhaev <pluhmylkin@me.com>
2018-10-28 10:55:12 +0700

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,18 @@ import Header from './components/Header';
 
 import { name, menu, social } from './cfg/data.json';
 import Footer from './components/Footer';
+import { Container } from 'reactstrap';
 class App extends Component {
   render() {
     const { items } = menu;
     return (
-      <div>
+      <Container>
         <Header name={name} items={items} />
         {items.map(item => (
           <Section item={item} />
         ))}
         <Footer name={name} social={social} />
-      </div>
+      </Container>
     );
   }
 }
